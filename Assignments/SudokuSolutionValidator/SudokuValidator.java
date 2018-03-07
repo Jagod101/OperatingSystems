4c4689a235434267ffe18de9e32c4d0e1012c388
jagod101 <jagod101@mail.chapman.edu>
2018-03-07 01:49:33 -0800

    Added Comments to Code

--- Assignments/SudokuSolutionValidator/SudokuValidator.java
+++ Assignments/SudokuSolutionValidator/SudokuValidator.java
@@ -3,17 +3,19 @@ import java.util.*;
 
 public class SudokuValidator {
 
+  //Initalize Sudoku Board as a 9x9 Multidimensional Array
   private static int board[][] = new int[9][9];
+  //Set Values to -1 to be true zero numbers
   private static int MRowNum = -1;
   private static int MColNum = -1;
   private static int MSubGridNum = -1;
-  private static int cError;
+  //Set Msgs to empty strings
   private static String MsgRow = " ";
   private static String MsgCol = " ";
   private static String MsgSubGrid = " ";
 
+  //Read file input and transpose data from .txt file
   private static void readBoard(String fileName) {
-
     try {
       BufferedReader br = new BufferedReader(new FileReader(fileName));
       String line;
@@ -31,13 +33,11 @@ public class SudokuValidator {
     catch (IOException io) {
       io.printStackTrace();
     }
-  printGrid();
+  printBoard();
   }
 
-  private static void printGrid() {
-    int SGCCounter = 0;
-    int SGRCounter = 0;
-
+  //Print Board
+  private static void printBoard() {
     for (int x =0; x < 9; ++x) {
       for (int y =0; y < 9; ++y) {
         System.out.print(board[x][y] + "  ");
@@ -47,6 +47,7 @@ public class SudokuValidator {
     System.out.println();
   }
 
+  //Thread that Checks Columns for Errors
   static private class CheckColumns extends Thread {
 
     private int colError = -1;
@@ -55,11 +56,14 @@ public class SudokuValidator {
     public void run() {
       for (int x = 0; x < 9; ++x) {
         for (int y = 0; y < 9; ++y) {
+          //If Error is Found
           if(!colSet.add(board[x][y])) {
             colError = x;
             System.out.println("COLUMN THREAD\t\tFound Error @ Column " + (x + 1));
             Integer[] tempArray = colSet.toArray(new Integer[colSet.size()]);
             Arrays.sort(tempArray);
+
+            //Iterate through and find duplicate, find correct/suggest new value to replace the duplicate
             for(int z = 1; z < 10; z++)
             {
               if(Arrays.asList(tempArray).contains(z) == false)
@@ -82,6 +86,7 @@ public class SudokuValidator {
     }
   }
 
+  //Thread that Checks Rows for Errors
   static private class CheckRows extends Thread {
 
     private int rowError = -1;
@@ -90,11 +95,14 @@ public class SudokuValidator {
     public void run() {
       for (int x = 0; x < 9; ++x) {
         for (int y = 0; y < 9; ++y) {
+          //If Error is Found
           if(!rowSet.add(board[x][y])) {
             rowError = x;
             System.out.println("ROW THREAD\t\tFound Error @ Row " + (x + 1));
             Integer[] tempArray = rowSet.toArray(new Integer[rowSet.size()]);
             Arrays.sort(tempArray);
+
+            //Iterate through and find duplicate, find correct/suggest new value to replace the duplicate
             for(int z = 1; z < 10; z++)
             {
               if(Arrays.asList(tempArray).contains(z) == false)
@@ -117,16 +125,20 @@ public class SudokuValidator {
     }
   }
 
+  //Function to check each of the 9 Sub Grids for possible Errors
   private static void checkSubGrid(int rowStart, int rowStop, int columnStart, int columnStop, int subGridNum) {
 
-    HashSet<Integer> checkGrid = new HashSet<>();
+    HashSet<Integer> checkBoard = new HashSet<>();
 
     for(int x = rowStart; x < rowStop; ++x) {
       for(int y = columnStart; y < columnStop; ++y) {
-        if(!checkGrid.add(board[x][y])) {
+        //If Error is Found
+        if(!checkBoard.add(board[x][y])) {
+          public boolean foundError = true;
           System.out.println("SUBGRID " + subGridNum + "\t\tFound Error @ Row " + (x + 1) + ", Column " + (y + 1));
-          Integer[] tempArray = checkGrid.toArray(new Integer[checkGrid.size()]);
+          Integer[] tempArray = checkBoard.toArray(new Integer[checkBoard.size()]);
 
+          //Iterate through and find duplicate, find correct/suggest new value to replace the duplicate
           for(int z = 1; z < 10; z++) {
             if(Arrays.asList(tempArray).contains(z) == false) {
               MSubGridNum += z + 1;
@@ -137,12 +149,13 @@ public class SudokuValidator {
           }
           x = rowStop;
           y = columnStop;
-          checkGrid.clear();
+          checkBoard.clear();
         }
       }
     }
   }
 
+  //Thread that separates each Sub Grid respectivley, calls function checkSubGrid
   static private class SubGridCheck extends Thread {
     public void run() {
       checkSubGrid(0,3,0,3,1);
@@ -157,17 +170,22 @@ public class SudokuValidator {
     }
   }
 
+  //Main Thread/Function
   public static void main(String args[]) {
+    //User Provides File Input
     System.out.print("INPUT File Name: ");
     //Set the Scanner
     Scanner sc = new Scanner(System.in);
     String fileName = sc.next();
 
     System.out.println();
+    //Calls Function readBoard that transposes data from file
     readBoard(fileName);
 
+    //Creates an ArrayList for Threads being used in program
     ArrayList<Thread> threads = new ArrayList<>();
 
+    //Three Threads, 1) Column, 2) Row, and 3) SubGrid will be ran
     CheckColumns columnThread = new CheckColumns();
     CheckRows rowThread = new CheckRows();
     SubGridCheck subGridThread = new SubGridCheck();
@@ -176,10 +194,17 @@ public class SudokuValidator {
     threads.add(new Thread(rowThread));
     threads.add(new Thread(subGridThread));
 
-    for(Thread t : threads) {
-      t.start();
+    //Try to Start all the Threads from Main
+    try {
+      for(Thread t : threads) {
+        t.start();
+      }
+    }
+    catch(Exception e) {
+      System.out.println("Unable To Start Threads");
     }
 
+    //Try to Join all the Threads back in Main
     try {
       for(Thread t : threads) {
         t.join();
@@ -189,10 +214,13 @@ public class SudokuValidator {
       e.printStackTrace();
     }
 
+    //If Errors are returned from Column
     int cError = columnThread.getColError();
 
+    //If Errors are returned from Row
     int rError = rowThread.getRowError();
 
+    //If neither Column or Row return Errors
     if(cError == -1 && rError == -1) {
       System.out.println("Sudoku Board Contains Zero Errors");
     }
